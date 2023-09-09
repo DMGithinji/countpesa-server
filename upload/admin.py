87d402477a087f78791054b237e0537a1bf321f9
Denis Githinji <dmwaigithinji@gmail.com>
2023-09-09 14:51:10 +0300

    show table in model

--- upload/admin.py
+++ upload/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UploadedFile
 
-# Register your models here.
+@admin.register(UploadedFile)
+class UploadedFileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content')
